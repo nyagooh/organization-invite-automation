9f323318fbfb7d7d7fb269f69097e674d00813df
nymaina <annmaina614@gmail.com>
2025-02-27 10:58:02 +0300

    add inviteuser function that invites user with they username and gives them role of member

--- utils/invite.go
+++ utils/invite.go
@@ -28,3 +28,25 @@ func ShouldInviteUser(ctx context.Context, client *github.Client, org, username 
 	return true, nil
 }
 
+func InviteUser(ctx context.Context, client *github.Client, org, username string) error {
+
+	user, _, err := client.Users.Get(ctx, username)
+	if err != nil {
+		return fmt.Errorf("error fetching user %s: %v", username, err)
+	}
+
+
+	inviteOptions := &github.CreateOrgInvitationOptions{
+		InviteeID: user.ID,
+		Role:      github.String("direct_member"),
+	}
+
+	
+	invitation, resp, err := client.Organizations.CreateOrgInvitation(ctx, org, inviteOptions)
+	if err != nil {
+		return fmt.Errorf("error sending invitation to %s: %v (HTTP status: %d)", username, err, resp.StatusCode)
+	}
+
+	fmt.Printf("Invitation sent to %s (Invitation ID: %d)\n", username, *invitation.ID)
+	return nil
+}
